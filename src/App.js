73eb27a0326d2806bea03667ebb41138accc485a
Dhallu <Dhallu@optymyze.com>
2017-08-07 13:12:19 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
     <div id="app">
@@ -12,5 +11,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
